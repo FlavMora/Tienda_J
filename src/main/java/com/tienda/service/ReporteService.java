3a9fd38d1b50b3c3433a84fc757ed2d7d9920467
FlavMora <franciscomh98@gmail.com>
2025-04-16 19:51:28 -0600

    Reportes Jasper

---
+++ src/main/java/com/tienda/service/ReporteService.java
@@ -0,0 +1,23 @@
+
+/*
+
+package com.tienda.service;
+import java.io.IOException;
+import java.util.Map;
+import org.springframework.core.io.Resource;
+import org.springFramework.http.ResponseEntity;
+
+/**
+ *
+ * @author franm
+ */
+/*public interface ReporteService {
+Public ResponseEntity <Resource> generaReporte(){
+        String reporte, 
+                Map<String, Object> parametros,
+                String tipo
+}
+        throw IOException;
+    
+}
+*/
